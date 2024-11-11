f8e6f939f527e261c75e0ba3f587b28b23fc477c
root <root@DESKTOP-J2KQ7OE>
2024-11-11 16:13:11 +0900

    로딩 스켈레톤 개선

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -33,8 +33,19 @@ async function BlogPost({ slug }: { slug: string }) {
   );
 }
 
+// 인위적인 딜레이를 추가하는 함수
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export default async function BlogPostPage(props: Props) {
+  // 테스트를 위한 3초 딜레이
+//   await delay(3000);
+  
   const params = await props.params;
+  const post = await getPostBySlug(params.slug);
+  
+  if (!post) {
+    notFound();
+  }
 
   return (
     <Suspense fallback={<PostLoading />}>
